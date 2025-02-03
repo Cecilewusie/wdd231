ff79262dab8631c42d0c4d2f76cc1e58a156a486
Cecilewusie <ewu22001@byui.edu>
2025-02-03 02:44:21 +0000

    new hamburger file

---
+++ chamber/scripts/hamburger.js
@@ -0,0 +1,18 @@
+// humberger menu
+const hamburgerMenu = document.querySelector(".menu");
+const navB = document.querySelector("#animatemenu");
+
+// adding event listners to hamburger menu
+hamburgerMenu.addEventListener('click', () => {
+    hamburgerMenu.classList.toggle("show");
+    navB.classList.toggle("show");
+});
+
+const navLinks = document.querySelectorAll("#header-nav a");
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        navLinks.forEach(navLink => navLink.classList.remove("active"));
+        event.target.classList.add("active");
+    });
+});
